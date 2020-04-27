bfa4604475fd197248ffe075f808ef88c9f3e752
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-04-27 10:35:12 +0200

    Part2: elegant way to be unsubscribed from the sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {data$, IProduct} from "./common/mock/data";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  public logo = 'assets/img/logo.png';
+  public products;
+  public subscription;
+  public control$$ = new Subject();
 
-  public logo = 'assets/img/logo.png'
+  ngOnInit(): void {
+    this.subscription = data$.pipe(takeUntil(this.control$$)).subscribe((products: IProduct[]) => {
+      this.products = products;
+    });
+  }
 
+  ngOnDestroy(): void {
+    this.control$$.next(true);
+  }
 
-  public text = ''
 }
