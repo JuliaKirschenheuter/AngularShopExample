0243f7e678a40340b6ebb233ea813feaf2765810
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-04-27 10:58:01 +0200

    Part2: pipe async and json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {data$, IProduct} from "./common/mock/data";
 import {Subject} from "rxjs";
-import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent {
   public logo = 'assets/img/logo.png';
-  public products;
+  public products = data$;
   public subscription;
   public control$$ = new Subject();
 
-  ngOnInit(): void {
-    this.subscription = data$.pipe(takeUntil(this.control$$)).subscribe((products: IProduct[]) => {
-      this.products = products;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.control$$.next(true);
-  }
 
 }
