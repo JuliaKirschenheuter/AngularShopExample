633090243415aa2533e4bb8b1cf812f85305581f
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-04-27 11:37:38 +0200

    Part2: directive *ngFor

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IProduct} from "../common/mock/data";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  public product: IProduct;
+
+}
