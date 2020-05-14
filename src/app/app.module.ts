68362d58acc51db8c057a8ce5e81bcb2c11ccc75
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-05-14 14:58:46 +0200

    Part5: navigation to single product was done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './content/products/product-list/cart/cart.component';
 import { ProductsFilterPipe } from './products-filter.pipe';
 import { ToUsdPipe } from './to-usd.pipe';
 import { TooltipDirective } from './common/directives/tooltip/tooltip.directive';
---
+++ src/app/content/products/product-list/cart/cart.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IProduct} from "../../../../store/reducers/products.reducer";
+import {Store} from "@ngrx/store";
+import {IStore} from "../../../../store";
+import {AddProductToCart} from "../../../../store/actions/cart.action";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  public constructor(
+    private _store: Store<IStore>
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  public product: IProduct;
+
+  @Input()
+  public position: number;
+
+  @Input()
+  public isOdd: boolean;
+
+  public addProductToCart(product: IProduct): void {
+    this._store.dispatch(new AddProductToCart(product))
+  }
+
+}
