a730dfa095ab9d1ba3b10da516bf505d47e903d1
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-05-20 10:22:46 +0200

    Part6: FormGroup inside of FormGroup was created from component

--- src/app/content/signup/signup.component.ts
+++ src/app/content/signup/signup.component.ts
@@ -19,7 +19,11 @@ export class SignupComponent implements OnInit {
 
     this.customForm = new FormGroup({
       firstName: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      lastName: new FormControl('', [Validators.required, Validators.minLength(6)])
+      lastName: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordGroup: new FormGroup({
+        password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+        cpassword: new FormControl('', [Validators.required, Validators.minLength(4)])
+      })
     });
 
     this._cd.detectChanges();
